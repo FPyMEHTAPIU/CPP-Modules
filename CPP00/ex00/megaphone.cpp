60a1a8ba4f673209a0da18e0af3b6e70ebb6bb6d
Nick Saveliev <kolya59264@gmail.com>
2025-05-12 07:00:17 +0300

    Change printing.

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 
-void print_upper_str(std::string &str) {
+static std::string str_to_upper(std::string &str) {
+	std::string upperStr = "";
+
 	for (char c : str) {
-		std::cout << static_cast<char>(toupper(c));
+		upperStr += static_cast<char>(toupper(c));
 	}
+	return (upperStr);
 }
 
 int main(int ac, char **av) {
+	std::string strToPrint = "";
+
 	if (ac == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
 	for (int i = 1; i < ac; ++i) {
 		std::string str = static_cast<std::string>(av[i]);
-		print_upper_str(str);
+		strToPrint += str_to_upper(str);
 	}
-	std::cout << std::endl;
+	std::cout << strToPrint << std::endl;
 	return (0);
 }

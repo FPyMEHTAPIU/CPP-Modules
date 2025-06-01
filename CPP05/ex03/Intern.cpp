b8966d58ed4a01e4146dbf0fc3bf6f0c1b29d93c
Nick Saveliev <kolya59264@gmail.com>
2025-06-01 21:15:10 +0300

    Update CPP05/ex03.

--- CPP05/ex03/Intern.cpp
+++ CPP05/ex03/Intern.cpp
@@ -1,5 +1,20 @@
 #include "Intern.hpp"
 
+namespace 
+{
+	const std::string requests[3] = {
+		"shrubbery creation",
+		"robotomy request",
+		"presidential pardon"
+	};
+
+	AForm* (*const formArr[3])(const std::string&) = {
+		[](const std::string& target) -> AForm* { return new ShrubberyCreationForm(target); },
+		[](const std::string& target) -> AForm* { return new RobotomyRequestForm(target); },
+		[](const std::string& target) -> AForm* { return new PresidentialPardonForm(target); }
+	};
+}
+
 Intern::Intern() {}
 Intern::~Intern() {}
 
@@ -8,18 +23,6 @@ const char* Intern::FormNotFound::what() const throw() {
 }
 
 AForm* Intern::makeForm(std::string formName, std::string targetName) {
-	const std::string requests[3] = {
-		"shrubbery creation",
-		"robotomy request",
-		"presidential pardon"
-	};
-
-	AForm* (*formArr[3])(const std::string&) = {
-		[](const std::string& target) -> AForm* { return new ShrubberyCreationForm(target); },
-		[](const std::string& target) -> AForm* { return new RobotomyRequestForm(target); },
-		[](const std::string& target) -> AForm* { return new PresidentialPardonForm(target); }
-	};
-
 	for (int i = 0; i < 3; ++i) {
 		if (formName == requests[i]) {
 			std::cout << "Intern creates " << formName << std::endl;
